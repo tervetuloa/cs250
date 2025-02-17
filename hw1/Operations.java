9de99ba4fbc24671aeedb83c194e63945bdfa7f9
tervetuloa <nab0nbiz@gmail.com>
2025-02-17 00:58:58 +0000

    task2

--- hw1/Operations.java
+++ hw1/Operations.java
@@ -1,6 +1,5 @@
 package cs250.hw1;
 
-
 public class Operations {
 
     public static void main(String[] args) {
@@ -14,12 +13,28 @@ public class Operations {
                 System.out.println("Incorrect number of arguments have been provided. Program Terminating!");
                 return;
             }
+        System.out.println("Task 2");
+        
+        String num1Type = whichNumberSystem(args[0]);
+        String num2Type = whichNumberSystem(args[1]);
+        String num3Type = whichNumberSystem(args[3]);
+
+        System.out.println(args[0] +  "=" + num1Type);
+        System.out.println(args[1] +  "=" + num2Type);
+        System.out.println(args[2] +  "=" + num3Type);
             
         
     }
-    public static String whichNumberSystem(String number)
-    {
-        if number.startsWith(0b)
+    public static String whichNumberSystem(String number) {
+        if (number.startsWith("0b")) {
+            return "Binary";
+        }
+        else if (number.startsWith("0x")) {
+            return "Hexadecimal";
+        }
+        else {
+            return "Decimal";
+        }
     }
 
 
