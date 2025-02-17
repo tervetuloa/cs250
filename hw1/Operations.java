2f97db7739e2fcadd922f24c1c01b9b9123bec1c
tervetuloa <nab0nbiz@gmail.com>
2025-02-17 00:43:03 +0000

    fixed

--- hw1/Operations.java
+++ hw1/Operations.java
@@ -1,12 +1,15 @@
-public static void main(String[] args) {
-    System.out.print("Task 1");
-    if (args.length == 3)
-        {
-            System.out.println("Correct number of arguments given.");
-        }   
-    else if (args.length > 3 || args.length < 3)
-        {
-            System.out.println("Incorrect number of arguments have been provided. Program Terminating!");
-            return;
-        }
+public class Operations {
+
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        if (args.length == 3)
+            {
+                System.out.println("Correct number of arguments given.");
+            }   
+        else 
+            {
+                System.out.println("Incorrect number of arguments have been provided. Program Terminating!");
+                return;
+            }
+    }
 }
