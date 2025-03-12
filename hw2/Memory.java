df8b4d5912433d46897c3fcab6a3a017311e70f8
Nathan Anderson <nab0nbiz@gmail.com>
2025-03-12 22:45:51 +0000

    task2?

--- hw2/Memory.java
+++ hw2/Memory.java
@@ -1,5 +1,7 @@
 package cs250.hw2;
 
+import java.util.Random;
+
 public class Memory {
 
     private static volatile int volatileLoopVariable;
@@ -34,13 +36,62 @@ public class Memory {
         double avgRegularTime = (regularTime / (double) experiments) / 1_000_000_000.0;
         double avgVolatileTime = (volatileTime / (double) experiments) / 1_000_000_000.0;
 
-        // Output results
+        
+
+        Integer[] array = new Integer[size];
+        Random random = new Random(seed);
+        for(int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+
+        long timeKnown = 0;
+        long timeRandom = 0;
+        long sum = 0;
+
+
+        for (int i = 0; i < experiments; i++) {
+            int knownIndex = size / 10;
+            long startTime = System.nanoTime();
+            int knownElement = array[knownIndex];
+            long endTime = System.nanoTime();
+
+            timeKnown += (endTime - startTime);
+            sum+= knownElement;
+
+
+        
+
+            int randomIndex= size - 1 - random.nextInt(size/10);
+            int randomElement = array[knownIndex];
+            endTime = System.nanoTime();
+            timeRandom += (endTime - startTime);
+            sum+= randomElement;
+        }
+
+        double avgTimeKnown = (double) timeKnown / experiments;
+        double avgTimeRandom = (double) timeRandom / experiments;
+
+
+
+
+
+        // Task 1 output
         System.out.println("Task 1");
         System.out.printf("Regular: %.5f seconds%n", avgRegularTime);
         System.out.printf("Volatile: %.5f seconds%n", avgVolatileTime);
         System.out.printf("Avg regular sum: %.2f%n", (double) regularSum / experiments);
         System.out.printf("Avg volatile sum: %.2f%n", (double) volatileSum / experiments);
+
+
+        // TYask 2 output
+        System.out.println("Task2");
+        System.out.printf("Avg time to access known element: %.2f nanoseconds%n", avgTimeKnown);
+        System.out.printf("Avg time to access random element: %.2f nanoseconds%n", avgTimeRandom);
+        System.out.printf("Sum: %.2f%n", (double) sum);
+
     }
+
+
 
     
     private static long runRegularLoop(int size) {
